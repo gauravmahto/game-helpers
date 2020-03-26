f60e4f6d787c34183e12ea9b4b0d418f989f93e1
Gaurav Mahto <gaurav.mahto@aristocrat.com>
2020-03-26 15:34:46 +0530

    Added test and debug config

---
+++ test.js
@@ -0,0 +1,53 @@
+// async function test() {
+
+//   return new Promise(async (res, rej) => {
+
+//     throw 'Error new';
+
+//     res(false);
+
+//   });
+
+// }
+
+// (async () => {
+
+//   try {
+//     await test();
+//   } catch (err) {
+//     console.log(err);
+//   }
+
+// })();
+
+const wait = (i, ms) => new Promise(resolve => setTimeout(() => resolve(i), ms));
+
+// Implementation One (Using for-loop)
+// const printNumbers = () => new Promise((resolve) => {
+//   let pr = Promise.resolve(0);
+//   for (let i = 1; i <= 10; i += 1) {
+//     pr = pr.then((val) => {
+//       console.log(val);
+//       return wait(i, Math.random() * 1000);
+//     });
+//   }
+//   resolve(pr);
+// });
+// printNumbers();
+
+// Implementation Two (Using Recursion)
+
+const printNumbersRecursive = () => {
+  return Promise.resolve(0).then(function processNextPromise(i) {
+
+    if (i === 10) {
+      return undefined;
+    }
+
+    return wait(i, Math.random() * 1000).then((val) => {
+      console.log(val);
+      return processNextPromise(i + 1);
+    });
+  });
+};
+printNumbersRecursive();
